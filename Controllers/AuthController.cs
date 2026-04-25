9edce57f7ef3e185d1725bb53995c47a63733bed
EnginHoca <enginniyazihoca@gmail.com>
2026-04-25 13:38:07 +0300

    tokenservices added.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using LibraryApi.Models;
+using LibraryApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+
+		private readonly ITokenService _tokenService;
+
+		public AuthController(ITokenService tokenService)
+		{
+			_tokenService = tokenService;
+		}
+
+
+		private static readonly List<User> _users = new()
+		{
+			new User{Id=1,Username="efeok",Password="1234",Role="admin" },
+			new User{Id=1,Username="halisgokce",Password="1234",Role="user" },
+			new User{Id=1,Username="doga",Password="1234",Role="user" },
+			new User{Id=1,Username="stajyerdeniz",Password="1234",Role="admin" }
+		};
+
+
+	}
+}
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,12 +1,56 @@
 ﻿using LibraryApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace LibraryApi.Services
 {
 	public class TokenService : ITokenService
 	{
+		private readonly JwtSettings _jwtSettings;
+
+		public TokenService(JwtSettings jwtSettings)
+		{
+			_jwtSettings = jwtSettings;
+		}
+
 		public (string Token, DateTime ExpiresAt) GenerateToken(User user)
 		{
-			throw new NotImplementedException();
+			// Token içerisine yazacagımız bilgileri (Claims)
+			var claims = new List<Claim>
+			{
+				new Claim(JwtRegisteredClaimNames.Sub,user.Id.ToString()),
+				new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+				new Claim(ClaimTypes.Name,user.Username),
+				new Claim(ClaimTypes.Role,user.Role)
+			};
+
+			//İmza için kullanılacak gizli anahtarı hazırlayalom 
+
+			var keyBytes = Encoding.UTF8.GetBytes(_jwtSettings.SecretKey);
+			var key = new SymmetricSecurityKey(keyBytes);
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			//Token Bitiş Zamanı
+
+			var expiresAt = DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryInMinutes);
+
+			//Token Olustur
+
+			var token = new JwtSecurityToken(
+                    issuer: _jwtSettings.Issuer,
+                    audience: _jwtSettings.Audience,
+                    claims: claims,
+                    expires: expiresAt,
+                    signingCredentials: creds);
+
+			//string çevir
+
+			var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+			return (tokenString,expiresAt);
+
 		}
 	}
 }
